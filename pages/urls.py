614f9132f1d433a4d6697e97aac85350c64a4131
ahmed13m <musheab13@gmail.com>
2023-01-30 12:53:51 +0530

    initial commit

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import homepage
+from .views import homepage, mainpage
 
 urlpatterns = [
-    path('',homepage,name='home')
+    path('',homepage,name='home'),
+    path('main/',mainpage.as_view(),name= 'main')
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 # Create your views here.
 
 def homepage(request):
     return HttpResponse("Hello World!")
+
+class mainpage(TemplateView):
+    template_name = "index.html"
+    
